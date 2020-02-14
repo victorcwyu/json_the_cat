f9009fb0c5e6c4d70618e4d2a161befafeed04b5
victorcwyu <victorcwyu@gmail.com>
2020-02-14 00:39:26 +0000

    Refactored and added tests to json_the_cat solution!

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,19 @@
 const request = require('request');
-const args = process.argv.slice(2);
-const breed = args[0];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(url, (error, response, body) => {
-  if (error) throw error;
-  const data = JSON.parse(body);
-  const breed = data[0];
-  if (breed) {
-    console.log(data[0].description);
-  } else {
-    console.log("You have not entered a valid breed of cat!");
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(url, (error, response, body) => {
+    if (response && response.statusCode === 200) {
+      const data = JSON.parse(body);
+      const breed = data[0];
+      if (breed) {
+        return callback(null, breed.description.trim());
+      } else {
+        return callback(null, "Breed not found!");
+      }
+    } return callback("There is a problem with your inputted URL!");
+  });
+};
+
+module.exports = { fetchBreedDescription };
---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const { fetchBreedDescription } = require('./breedFetcher.js');
+
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
